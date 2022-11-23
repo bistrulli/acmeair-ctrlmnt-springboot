f3dc18d2098ea63fe69e2f893aaf46a48d4e4a2f
emilio-imt <emilio-imt@MacBook-Pro.homenet.telecomitalia.it>
2022-11-23 22:26:37 +0100

    added doWork method

--- src/main/java/ctrlmnt/ControllableService.java
+++ src/main/java/ctrlmnt/ControllableService.java
@@ -1,7 +1,62 @@
 package ctrlmnt;
 
-public interface ControllableService {
-	public Float getHw();
-	public void setHw(Float hw);
-	public String getName();
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.math3.distribution.ExponentialDistribution;
+
+public abstract class ControllableService {
+
+	public abstract Float getHw();
+	public abstract void setHw(Float hw);
+	public abstract String getName();
+	public abstract void ingress();
+	public abstract void egress();
+	public abstract Integer getUser();
+
+	public void doWork(long stime) {
+		this.ingress();
+		try {
+			ExponentialDistribution dist = new ExponentialDistribution(stime);
+			Double isTime = dist.sample();
+
+			Double hwkm1 = null;
+			Double usersKm1 = null;
+			Double d = null;
+
+			usersKm1 = this.getUser().doubleValue();
+			hwkm1 = this.getHw().doubleValue();
+			if (usersKm1 >= hwkm1) {
+				d = (double) (isTime.doubleValue() * (usersKm1 / hwkm1));
+			} else {
+				d = isTime.doubleValue();
+			}
+
+			while (d > 0) {
+				long st = System.nanoTime();
+				TimeUnit.MILLISECONDS.sleep(Math.min(d.longValue(), 10l));
+				long wt = (System.nanoTime() - st);
+				d -= wt / 1.0e6;
+
+				if (usersKm1 >= hwkm1) {
+					isTime = d * (hwkm1 / usersKm1);
+				} else {
+					isTime = d;
+				}
+
+				usersKm1 = this.getUser().doubleValue();
+				hwkm1 = this.getHw().doubleValue();
+				if (usersKm1 >= hwkm1) {
+					d = (double) (isTime.doubleValue() * (usersKm1 / hwkm1));
+				} else {
+					d = isTime.doubleValue();
+				}
+
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			this.egress();
+		}
+
+	}
 }
--- src/main/java/ctrlmnt/CtrlMNT.java
+++ src/main/java/ctrlmnt/CtrlMNT.java
@@ -1,34 +1,23 @@
 package ctrlmnt;
 
-import org.bson.Document;
-
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
+import redis.clients.jedis.Jedis;
 
 public class CtrlMNT implements Runnable {
 
 	private ControllableService svc = null;
-	private MongoClient mongo = null;
-	private MongoDatabase msDB = null;
+	private Jedis jedis=null;
 
 	public CtrlMNT(ControllableService serviceRest) {
 		this.svc = serviceRest;
-		this.mongo = MongoClients.create("mongodb://localhost:27017");
-		this.msDB = this.mongo.getDatabase("sys");
+		this.jedis=new Jedis();
 	}
 
-	public void run() { 
-		MongoCollection<Document> msCol = this.msDB.getCollection("ms");
-		FindIterable<Document> ms = msCol.find(Filters.eq("name", this.svc.getName().trim().strip()));
-		Document msDoc = null;
-		if ((msDoc = ms.first()) != null) {
-			this.svc.setHw(msDoc.getDouble("hw").floatValue());
-		} else {
-			//System.out.println("mircoservice"+this.svc.getName()+"not found"); 
+	public void run() {
+		String hw=this.jedis.get(this.svc.getName()+"_"+"hw");
+		if(hw!=null) {
+			this.svc.setHw(Float.valueOf(hw));
+		}else {
+			//System.out.println("null");
 		}
 	}
 }
