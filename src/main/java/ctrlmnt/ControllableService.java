b09c4cf508c9f70eba099c800a69e8f153f1654f
emilio-imt <emilio-imt@MacBook-Pro.local>
2023-01-27 22:29:08 +0100

    minor

--- src/main/java/ctrlmnt/ControllableService.java
+++ src/main/java/ctrlmnt/ControllableService.java
@@ -17,80 +17,80 @@ public abstract class ControllableService {
 
 	public abstract Integer getUser();
 
-//	public void doWork(long stime) {
-//		//TODO devo aggiungere la logica per eseguire il compito sulla cpu e non solo attraverso gli sleep
-//		this.ingress();
-//		try {
-//			ExponentialDistribution dist = new ExponentialDistribution(stime);
-//			Double isTime = dist.sample();
-//
-//			Double hwkm1 = null;
-//			Double usersKm1 = null;
-//			Double d = null;
-//
-//			usersKm1 = this.getUser().doubleValue();
-//			hwkm1 = this.getHw().doubleValue();
-//			if (usersKm1 >= hwkm1) {
-//				d = (double) (isTime.doubleValue() * (usersKm1 / hwkm1));
-//			} else {
-//				d = isTime.doubleValue();
-//			}
-//
-//			while (true) {
-//				long st = System.nanoTime();
-//				TimeUnit.MILLISECONDS.sleep(Math.min(d.longValue(), 20l));
-//				long wt = (System.nanoTime() - st);
-//				d -= wt / 1.0e6;
-//				
-//				if(d<=0) {
-//					break;
-//				}
-//
-//				if (usersKm1 >= hwkm1) {
-//					isTime = d * (hwkm1 / usersKm1);
-//				} else {
-//					isTime = d;
-//				}
-//
-//				usersKm1 = this.getUser().doubleValue();
-//				hwkm1 = this.getHw().doubleValue();
-//				if (usersKm1 >= hwkm1) {
-//					d = (double) (isTime.doubleValue() * (usersKm1 / hwkm1));
-//				} else {
-//					d = isTime.doubleValue();
-//				}
-//
-//			}
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		} finally {
-//			this.egress();
-//		}
-//	}
-
 	public void doWork(long stime) {
+		//TODO devo aggiungere la logica per eseguire il compito sulla cpu e non solo attraverso gli sleep
 		this.ingress();
 		try {
 			ExponentialDistribution dist = new ExponentialDistribution(stime);
 			Double isTime = dist.sample();
 
-			double usersKm1 = this.getUser().doubleValue();
-			double hwkm1 = this.getHw().doubleValue();
-			
-			//Double isTime=Long.valueOf(stime).doubleValue();
+			Double hwkm1 = null;
+			Double usersKm1 = null;
 			Double d = null;
-			if (usersKm1 > hwkm1) {
+
+			usersKm1 = this.getUser().doubleValue();
+			hwkm1 = this.getHw().doubleValue();
+			if (usersKm1 >= hwkm1) {
 				d = (double) (isTime.doubleValue() * (usersKm1 / hwkm1));
 			} else {
 				d = isTime.doubleValue();
 			}
 
-			TimeUnit.MILLISECONDS.sleep(d.longValue());
+			while (true) {
+				long st = System.nanoTime();
+				TimeUnit.MILLISECONDS.sleep(Math.min(d.longValue(), 20l));
+				long wt = (System.nanoTime() - st);
+				d -= wt / 1.0e6;
+				
+				if(d<=0) {
+					break;
+				}
 
+				if (usersKm1 >= hwkm1) {
+					isTime = d * (hwkm1 / usersKm1);
+				} else {
+					isTime = d;
+				}
+
+				usersKm1 = this.getUser().doubleValue();
+				hwkm1 = this.getHw().doubleValue();
+				if (usersKm1 >= hwkm1) {
+					d = (double) (isTime.doubleValue() * (usersKm1 / hwkm1));
+				} else {
+					d = isTime.doubleValue();
+				}
+
+			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} finally {
 			this.egress();
 		}
 	}
+
+//	public void doWork(long stime) {
+//		this.ingress();
+//		try {
+//			ExponentialDistribution dist = new ExponentialDistribution(stime);
+//			Double isTime = dist.sample();
+//
+//			double usersKm1 = this.getUser().doubleValue();
+//			double hwkm1 = this.getHw().doubleValue();
+//			
+//			//Double isTime=Long.valueOf(stime).doubleValue();
+//			Double d = null;
+//			if (usersKm1 > hwkm1) {
+//				d = (double) (isTime.doubleValue() * (usersKm1 / hwkm1));
+//			} else {
+//				d = isTime.doubleValue();
+//			}
+//
+//			TimeUnit.MILLISECONDS.sleep(d.longValue());
+//
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		} finally {
+//			this.egress();
+//		}
+//	}
 }
