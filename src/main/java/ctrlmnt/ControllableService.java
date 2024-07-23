ad1e0eecad6c984723aed6720c44863628e24ffd
EMI <183048+bistrulli@users.noreply.github.com>
2024-07-23 12:05:18 +0200

    Fix dependencies

--- src/main/java/ctrlmnt/ControllableService.java
+++ src/main/java/ctrlmnt/ControllableService.java
@@ -1,25 +1,27 @@
 package ctrlmnt;
 
+import java.lang.management.ManagementFactory;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.lang.management.ThreadMXBean;
 
 import org.apache.commons.math3.distribution.ExponentialDistribution;
 
 public abstract class ControllableService {
 
-	public abstract Float getHw();
+    public abstract Float getHw();
 
-	public abstract void setHw(Float hw);
+    public abstract void setHw(Float hw);
 
-	public abstract String getName();
+    public abstract String getName();
 
-	public abstract void ingress();
+    public abstract void ingress();
 
-	public abstract void egress();
+    public abstract void egress();
 
-	public abstract Integer getUser();
+    public abstract Integer getUser();
 
-	public abstract String getIscgroup();
+    public abstract String getIscgroup();
 
     private ExponentialDistribution dist = null;
     private ThreadMXBean mgm = null;
@@ -34,56 +36,56 @@ public abstract class ControllableService {
         }
     }
 
-	public void doWorkSleep(long stime) {
+    public void doWorkSleep(long stime) {
 
-		if (this.getIscgroup() != "y") {
-			this.ingress();
-			try {
-				ExponentialDistribution dist = new ExponentialDistribution(stime);
-				Double isTime = dist.sample();
+        if (this.getIscgroup() != "y") {
+            this.ingress();
+            try {
+                ExponentialDistribution dist = new ExponentialDistribution(stime);
+                Double isTime = dist.sample();
 
-				Double hwkm1 = null;
-				Double usersKm1 = null;
-				Double d = null;
+                Double hwkm1 = null;
+                Double usersKm1 = null;
+                Double d = null;
 
-				usersKm1 = this.getUser().doubleValue();
-				hwkm1 = this.getHw().doubleValue();
-				if (usersKm1 >= hwkm1) {
-					d = (double) (isTime.doubleValue() * (usersKm1 / hwkm1));
-				} else {
-					d = isTime.doubleValue();
-				}
+                usersKm1 = this.getUser().doubleValue();
+                hwkm1 = this.getHw().doubleValue();
+                if (usersKm1 >= hwkm1) {
+                    d = (double) (isTime.doubleValue() * (usersKm1 / hwkm1));
+                } else {
+                    d = isTime.doubleValue();
+                }
 
-				while (true) {
-					long st = System.nanoTime();
-					TimeUnit.MILLISECONDS.sleep(Math.min(d.longValue(), 20l));
-					long wt = (System.nanoTime() - st);
-					d -= wt / 1.0e6;
+                while (true) {
+                    long st = System.nanoTime();
+                    TimeUnit.MILLISECONDS.sleep(Math.min(d.longValue(), 20l));
+                    long wt = (System.nanoTime() - st);
+                    d -= wt / 1.0e6;
 
-					if (d <= 0) {
-						break;
-					}
+                    if (d <= 0) {
+                        break;
+                    }
 
-					if (usersKm1 >= hwkm1) {
-						isTime = d * (hwkm1 / usersKm1);
-					} else {
-						isTime = d;
-					}
+                    if (usersKm1 >= hwkm1) {
+                        isTime = d * (hwkm1 / usersKm1);
+                    } else {
+                        isTime = d;
+                    }
 
-					usersKm1 = this.getUser().doubleValue();
-					hwkm1 = this.getHw().doubleValue();
-					if (usersKm1 >= hwkm1) {
-						d = (double) (isTime.doubleValue() * (usersKm1 / hwkm1));
-					} else {
-						d = isTime.doubleValue();
-					}
+                    usersKm1 = this.getUser().doubleValue();
+                    hwkm1 = this.getHw().doubleValue();
+                    if (usersKm1 >= hwkm1) {
+                        d = (double) (isTime.doubleValue() * (usersKm1 / hwkm1));
+                    } else {
+                        d = isTime.doubleValue();
+                    }
 
-				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} finally {
-				this.egress();
-			}
-		}
-	}
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                this.egress();
+            }
+        }
+    }
 }
